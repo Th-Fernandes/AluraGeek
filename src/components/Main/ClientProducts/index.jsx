e86c20729eafe9380e9a4622728ec2f20a30fbc4
Thiago Fernandes <tjfctigo@gmail.com>
2022-04-09 18:12:21 -0300

    total json server refactoring

--- src/components/Main/ClientProducts/index.jsx
+++ src/components/Main/ClientProducts/index.jsx
@@ -3,21 +3,43 @@ import { StyledClientSec } from './styles'
 import React from 'react'
 import { loginController } from 'controller/isLogged'
 import ProductSection from "components/Main/ProductSection/ProductSection.jsx"
+import {useRouter} from "next/router"
 
 export default function ClientProducts() {
 
+  const router = useRouter()
+
   return (
-    <StyledClientSec>
-      <ClientHeader />
+    <>
+    {
+      loginController.userInfo 
+        ? (
+            <StyledClientSec>
+              <ClientHeader />
+        
+              <ProductSection
+                title=''
+                productData={loginController.userInfo}
+              />
+        
+        
+              {console.log(loginController.userInfo)}
+    
+            </StyledClientSec>
+        )
+       
+        : (
+          <>
+            Você precisa fazer login para acessar essa página
+            <button
+              onClick={() => router.push('./login')}
+            >fazer login</button>
+          </>
+        )
 
-      <ProductSection
-        title=''
-        productData={loginController.userInfo}
-      />
-
-
-      {console.log(loginController.userInfo.items)}
-
-    </StyledClientSec>
+      
+    }
+    </>
+    
   )
 }
