207a52956ca575cd331d492e0dc4ca2328b02a3c
Thiago Fernandes <tjfctigo@gmail.com>
2022-08-23 11:09:35 -0300

    negar acesso aba login user logado

--- pages/access/login.js
+++ pages/access/login.js
@@ -1,20 +1,37 @@
 import Head from 'next/head';
 import Header from "components/Header/Header";
-import {SignActionsSection} from "components/Main/SignActionsSection";
+import { SignActionsSection } from "components/Main/SignActionsSection";
 import Footer from "components/Footer/Footer";
+import { supabaseAuth } from "helpers/supabase-auth-actions";
+import { useRouter } from 'next/router'
 
 export default function LoginPage() {
+  const router = useRouter();
+  const isUserLogged = supabaseAuth.getSessionInfo();
+
+  function handleRedirectToHomePage() {
+    router.push('/')
+  }
+
   return (
     <>
-      <Head>
-        <title>AluraGeek - login</title>
-      </Head>
+      {
+        !isUserLogged
+          ? (
+            <>
+              <Head>
+                <title>AluraGeek - login</title>
+              </Head>
 
-      <Header />
-      <main>
-        <SignActionsSection />
-      </main>
-      <Footer />
+              <Header />
+              <main>
+                <SignActionsSection />
+              </main>
+              <Footer />
+            </>
+          )
+          : handleRedirectToHomePage()
+      }
     </>
   )
 }
